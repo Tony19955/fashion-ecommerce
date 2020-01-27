2b8d0867e19b1b4257e6acfd0789312de861c102
Mattias <mattias.mik@gmail.com>
2020-01-27 11:09:10 +0100

    Implemented code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page. Also included our WithSpinner HOC

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -1,8 +1,11 @@
 import React from 'react';
+
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
-import './sign-in.styles.scss';
+
+import { SignInContainer, SignInTitle, ButtonsBarContainer } from './sign-in.styles';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -11,7 +14,7 @@ class SignIn extends React.Component {
     this.state = {
       email: '',
       password: ''
-    }
+    };
   }
 
   handleSubmit = async event => {
@@ -30,39 +33,41 @@ class SignIn extends React.Component {
   handleChange = event => {
     const { value, name } = event.target;
 
-    this.setState({ [name]: value })
+    this.setState({ [name]: value });
   };
 
   render() {
-    return(
-      <div className='sign-in'>
-        <h2>I already have an account</h2>
+    return (
+      <SignInContainer>
+        <SignInTitle>I already have an account</SignInTitle>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <FormInput 
-            name="email" 
-            type="email" 
-            value={this.state.email} 
-            handleChange={this.handleChange} 
-            label="email"
-            required 
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required
           />
-          <FormInput 
-            name="password" 
-            type="password" 
-            value={this.state.password} 
-            handleChange={this.handleChange} 
-            label="password"
-            required 
+          <FormInput
+            name='password'
+            type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='password'
+            required
           />
-          <div className='buttons'>
-            <CustomButton type='submit'>Sign in</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
-          </div>
+          <ButtonsBarContainer>
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </ButtonsBarContainer>
         </form>
-      </div>
-    )
+      </SignInContainer>
+    );
   }
 }
 
--- src/components/sign-up/sign-up.component.js
+++ src/components/sign-up/sign-up.component.js
@@ -1,13 +1,15 @@
+
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+
 import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 
-import './sign-up.styles.scss';
+import { SignUpContainer, SignUpTitle } from './sign-up.styles';
 
 class SignUp extends React.Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
@@ -15,7 +17,7 @@ class SignUp extends React.Component {
       email: '',
       password: '',
       confirmPassword: ''
-    }
+    };
   }
 
   handleSubmit = async event => {
@@ -23,23 +25,25 @@ class SignUp extends React.Component {
 
     const { displayName, email, password, confirmPassword } = this.state;
 
-    if(password !== confirmPassword) {
+    if (password !== confirmPassword) {
       alert("passwords don't match");
       return;
     }
 
     try {
-      const { user } = await auth.createUserWithEmailAndPassword(email, password);
-      
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
       await createUserProfileDocument(user, { displayName });
-     
+
       this.setState({
         displayName: '',
         email: '',
         password: '',
         confirmPassword: ''
       });
-      
     } catch (error) {
       console.error(error);
     }
@@ -49,13 +53,13 @@ class SignUp extends React.Component {
     const { name, value } = event.target;
 
     this.setState({ [name]: value });
-  }
+  };
 
   render() {
     const { displayName, email, password, confirmPassword } = this.state;
-    return(
-      <div className='sign-up'>
-        <h2 className='title'>I do not have an account</h2>
+    return (
+      <SignUpContainer>
+        <SignUpTitle>I do not have a account</SignUpTitle>
         <span>Sign up with your email and password</span>
         <form className='sign-up-form' onSubmit={this.handleSubmit}>
           <FormInput
@@ -89,11 +93,11 @@ class SignUp extends React.Component {
             onChange={this.handleChange}
             label='Confirm Password'
             required
-          />            
+          />
           <CustomButton type='submit'>SIGN UP</CustomButton>
         </form>
-      </div>
-    )
+      </SignUpContainer>
+    );
   }
 }
 
